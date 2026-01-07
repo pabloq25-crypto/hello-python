bcb1e65273d8c1b54c8ed1db3e33d68d11750230
pabloq25-crypto <sanmiguelpabloq25@gmail.com>
2026-01-07 15:10:43 -0500

    Curso python, comienzo seccion 3 de strings

---
+++ 00_hello.py
@@ -0,0 +1,26 @@
+# Hola Mundo
+# Nuestro hola mundo
+print("Hola Python")
+print('Hola Python')
+
+
+
+"""
+Esto es un 
+comentario 
+en varias líneas.
+"""
+'''
+Esto también es un 
+comentario 
+en varias líneas.
+'''
+# Consultar el tipo de dato
+print(type('Hola python')) # tipo str
+print(type(5)) # tipo int 
+print(type(1.5)) # tipo float
+print(type(3 + 1j)) # tipo complex
+print(type(True)) # tipo bool
+
+
+print(type(print("Mi cadena de texto"))) #NoneType
---
+++ 02_operators.py
@@ -0,0 +1,43 @@
+### Operadores aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola " + "Python " + "¿qué tal?")
+print("Hola " + str(5))
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+print("")
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Zola") # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa")) #Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+print("")
+### Operadores Lógicos ###
+print(3 > 4 and"Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not(3 > 4))
+# print(3 > 4 not "Hola" > "Python")  
